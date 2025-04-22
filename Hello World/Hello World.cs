ce9d70dc0a01adbbaeef00a3da56b6ec9578267f
imrankhanv <imrankhanv@TRNE-IMRANV>
2025-04-22 15:16:06 +0530

    Add project files.

---
+++ Hello World/Hello World.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Consolevalue //used for avoid naming conflect
+{
+    class Hello //Blueprint of creating object
+    {
+        public static void Main(string[] args) //main method
+        {
+            Console.WriteLine("Happy world and good day"); //console line
+            //ouput ---> Happy world and good day
+        }
+    }
+}
