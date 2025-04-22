9c6d77e68ac6361e03295431c033797373158ea9
imrankhanv <imranfav7@gmail.com>
2025-04-22 16:57:01 +0530

    variable class

--- Hello World/Hello World.cs
+++ Hello World/Hello World.cs
@@ -6,7 +6,7 @@ namespace Consolevalue //used for avoid naming conflect
     {
         public static void Main(string[] args) //main method
         {
-            Console.WriteLine("Happy world and good day"); //console line
+            Console.WriteLine("Happy world and good day for all"); //console line
             //ouput ---> Happy world and good day
         }
     }
